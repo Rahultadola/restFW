7644b672fe8a9d6c64ea2b4169d5f6d00a838751
Rahul Tadola <rahultadola@gmail.com>
2023-04-10 21:27:26 +0530

    proper models added

--- FrameworkApp/models.py
+++ FrameworkApp/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils.translation import gettext_lazy as _
 
 
 # Create your models here.
@@ -20,7 +21,37 @@ class Education(models.Model):
 
 
 
-class PersonalInformation(models.model):
+class Sexuality(models.TextChoices):
+	MALE = "M", _("Male")
+	FEMALE = "F", _("Female")
+	TRANSGENDER = "T", _("Transgender")
+
+
+class Purpose(models.TextChoices):
+	CHAT = "C", _("Chat")
+	FRIENDSHIP = "F", _("Friendship")
+	DATE = "D", _("Date")
+	RELATIONSHIP = "R", _("Relationship")
+
+
+
+
+
+class ProfileImages(models.Model):
+	image_one = models.ImageField()
+	image_two = models.ImageField()
+	image_three = models.ImageField()
+	image_four = models.ImageField()
+	image_five = models.ImageField()
+	image_six = models.ImageField()
+
+	def __repr__(self):
+		return f'{self.id}'
+
+
+
+
+class PersonalInformation(models.Model):
 	f_name = models.CharField(max_length=50, null=False, blank=False)
 	l_name = models.CharField(max_length=50, null=False, blank=False)
 	email = models.EmailField()
@@ -34,11 +65,11 @@ class PersonalInformation(models.model):
 	instagram = models.CharField(max_length=100)
 	mobile_number = models.PositiveIntegerField(min=6000000000, max=9999999999)
 	mobile_number_verified = models.BooleanField(default=False)
-	sexuality = models.ChoiceField([])
-	purpose = models.ChoiceField([])
+	sexuality = models.CharField(max_length=1, choices=Sexuality.choices, default=Sexuality.MALE)
+	purpose = models.CharField(max_length=1, choices=Purpose.choices, default=Purpose.)
 	activity_interests = models.ChoiceField([])
 	profile_interest = models.CharField([])
-	images = models.ImageField()
+	images = models.ForeignKey('ProfileImages')
 	height = models.PositiveIntegerField()
 	children = models.ChoiceField([])
 	drinking = models.ChoiceField([])
@@ -55,11 +86,11 @@ class PersonalInformation(models.model):
 
 
 class Profile(models.Model):
-	user = models.ForeignKey(User, on_delete=models.CASCADE)
-	personal_information = models.ForeignKey(PersonalInformation, on_delete=models.CASCADE)	
+	user = models.ForeignKey('User', on_delete=models.CASCADE)
+	personal_information = models.ForeignKey('PersonalInformation', on_delete=models.CASCADE)	
 	coins = models.PositiveIntegerField(min=0, default=250)
-	likes_given = models.ManyToManyField(Profile)
-	likes_recieved = models.ManyToManyField(Profile)
+	likes_given = models.ManyToManyField('Profile')
+	likes_recieved = models.ManyToManyField('Profile')
 	visibility_area = models.PositiveIntegerField(min=0, max=40000)
 	active = models.BooleanField(default=True)
 
